b5521ec609bf3c544648d282141f4ac38f07035d
Vlad <vlad_x@MacBook-Air.local>
2026-06-20 16:30:55 -0400

    Sync Keept slice from website monorepo

--- src/keeptAdmin/KeeptAdminApp.tsx
+++ src/keeptAdmin/KeeptAdminApp.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Routes, Route, NavLink, useNavigate } from 'react-router-dom'
 import { ModerationPanel } from './ModerationPanel'
 import { SettingsPanel } from './SettingsPanel'
--- src/keeptAdmin/SettingsPanel.tsx
+++ src/keeptAdmin/SettingsPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { fetchProviderCatalog, ProviderCatalogModel } from './services/moderationApi'
 
 export const SettingsPanel: React.FC = () => {
